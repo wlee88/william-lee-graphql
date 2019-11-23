3e698dba3a3bbd69a99c9474b26de81411d39889
William Lee <me@william-lee.com>
2019-11-23 16:45:08 +1000

    fix: port number

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const server = new ApolloServer(
         resolvers
     });
 
-server.listen().then(({url}) => {
+server
+    .listen({port: process.env.PORT || 4000})
+    .then(({url}) => {
     console.log(`Server ready at ${url}`);
 });
