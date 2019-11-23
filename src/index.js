b801a6404f4a1c210ef777e6ce55e5f751195747
William Lee <me@william-lee.com>
2019-11-23 16:47:02 +1000

    fix: allow introspection in production

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const resolvers = require('./resolvers');
 const server = new ApolloServer(
     { 
         typeDefs,
-        resolvers
+        resolvers,
+        introspection: true
     });
 
 server
